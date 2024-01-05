250246bcd700be2930a3f2ec0d378ec8ced29687
@natayaa <ivan.azhari16@gmail.com>
2024-01-05 15:04:10 +0700

    modify logscan.py

--- app/endpoint/logscan.py
+++ app/endpoint/logscan.py
@@ -16,8 +16,10 @@ template = Jinja2Templates(directory="templates/")
 
 @logscan_page.get("/logscan-page", response_class=HTMLResponse)
 async def logscan_page_load(request: Request, search: str = None, limit: int = 10, offset: int = 0):
-    records_logs = scanner_logs.show_logs(search=search, limit=limit, offset=offset)
+    offsets = int((offset - 1) * limit)
+    records_logs = scanner_logs.show_logs(search=search, limit=limit, offset=offsets)
     retval_logscan = {"request": request, "title": TITLE_PAGE, "logscan_data": records_logs.get("data_logs"), 
-                      "total_records": records_logs.get("total_records")}
+                      "total_records": records_logs.get("total_records"),
+                      "limit": limit, "page": offset}
     
     return template.TemplateResponse("logscan.html", context=retval_logscan, status_code=status.HTTP_200_OK)
