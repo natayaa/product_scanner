ed957f1e9d66438dcaaf901068d69408167bd011
@natayaa <ivan.azhari16@gmail.com>
2024-01-15 07:55:21 +0700

    fixing pagination and ending the project

--- app/endpoint/logscan.py
+++ app/endpoint/logscan.py
@@ -18,8 +18,18 @@ template = Jinja2Templates(directory="templates/")
 async def logscan_page_load(request: Request, search: str = None, limit: int = 10, offset: int = 0):
     offsets = int((offset - 1) * limit)
     records_logs = scanner_logs.show_logs(search=search, limit=limit, offset=offsets)
-    retval_logscan = {"request": request, "title": TITLE_PAGE, "logscan_data": records_logs.get("data_logs"), 
-                      "total_records": records_logs.get("total_records"),
-                      "limit": limit, "page": offset}
+    
+    total_records = records_logs.get("total_records")
+    total_pages = (total_records + limit - 1) // limit
+    
+    retval_logscan = {
+        "request": request,
+        "title": TITLE_PAGE,
+        "logscan_data": records_logs.get("data_logs"),
+        "total_records": total_records,
+        "limit": limit,
+        "page": offset,
+        "total_pages": total_pages  # Add total_pages to the context
+    }
     
     return template.TemplateResponse("logscan.html", context=retval_logscan, status_code=status.HTTP_200_OK)
